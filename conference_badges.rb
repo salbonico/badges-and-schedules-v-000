93a3e2d8d31402d59ab39ebc1739c456fef074f7
Steve Albonico <salbonico@gmail.com>
2019-01-15 19:34:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(name)
+  name.each do |name|
+    return "Hello, my name is #{name}."
+  end
+
+def assign_rooms(name)
+  room = 0
+  room.each do |name|
+    room += 1
+    return "Hello, #{name}! You'll be assigned to room #{room}!"
